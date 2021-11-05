f7772fe58c16e8f936baadede883efcd28f25880
Anifyuli <anifyuliansyah@gmail.com>
2021-11-05 17:21:25 +0700

    Finishing user auth file

--- Ujian-Praktik/ujian-live/checklogin.php
+++ Ujian-Praktik/ujian-live/checklogin.php
@@ -7,7 +7,37 @@ if (isset($_POST['login'])) {
     $password = $_POST['password'];
 
     if ($username == "admin") {
-        $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username' AND password = '$password ")
+        $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username' AND password = '$password' ");
+
+        $data = mysqli_fetch_assoc($result);
+
+        if (mysqli_num_rows($result) == 1) {
+            $_SESSION['admin'] = true;
+            $_SESSION['user'] = admin;
+            $_SESSION['login'] = true;
+            header("Location: admin/home.php");
+            exit;
+        } else {
+            $_SESSION["gagal"] = true;
+            header("Location: login.php");
+        }
+    } else {
+        $result = mysqli_query($conn, "SELECT * FROM user WHERE username='$username' AND password='$password'");
+
+        $data = mysqli_fetch_assoc($result);
+
+        if (mysqli_num_rows($result) === 1) {
+            $_SESSION['user'] = true;
+            $_SESSION['admin'] = true;
+            $_SESSION["user"] = $username;
+            $_SESSION["login"] = true;
+            header(Location: user/home.php);
+            exit;
+        } else {
+            $_SESSION["gagal"] = true;
+            header(Location: login.php);
+        }
+
     }
 }
 
